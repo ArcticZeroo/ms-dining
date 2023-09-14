9904a01cdb245f79194818c734921a6d1fd31c00
Spencer <spencer@frozor.io>
2023-09-14 15:38:20 -0700

    Add canCollapse to support upcoming return button

--- client/src/components/dining-halls/collapsible-dining-hall-menu.tsx
+++ client/src/components/dining-halls/collapsible-dining-hall-menu.tsx
@@ -5,20 +5,29 @@ import { ConceptList } from './concept/concept-list.tsx';
 interface IHomePageDiningHallMenuProps {
     diningHall: IDiningHall;
     menu: DiningHallMenu;
+    canCollapse: boolean;
 }
 
-export const CollapsibleDiningHallMenu: React.FC<IHomePageDiningHallMenuProps> = ({ diningHall, menu }) => {
+export const CollapsibleDiningHallMenu: React.FC<IHomePageDiningHallMenuProps> = ({
+                                                                                      diningHall,
+                                                                                      menu,
+                                                                                      canCollapse
+                                                                                  }) => {
     const [isExpanded, setIsExpanded] = useState(true);
 
     return (
         <div className="combined-dining-hall" key={diningHall.id}>
             <button className="combined-dining-hall-title" onClick={() => setIsExpanded(!isExpanded)}>
                 {diningHall.name} Menu
-                <span className={`material-symbols-outlined expand-icon ${isExpanded ? 'expanded': 'collapsed'}`}>
-                    expand_more
-                </span>
+                {
+                    canCollapse && (
+                        <span className={`material-symbols-outlined expand-icon ${isExpanded ? 'expanded' : 'collapsed'}`}>
+                            expand_more
+                        </span>
+                    )
+                }
             </button>
-            <ConceptList concepts={menu} isVisible={isExpanded}/>
+            <ConceptList concepts={menu} isVisible={!canCollapse || isExpanded}/>
         </div>
     );
 };
