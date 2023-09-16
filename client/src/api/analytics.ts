bf754fa68886ae1d72a381bdc7fd90fce7c1cd6d
Spencer <spencer@frozor.io>
2023-09-15 22:29:29 -0700

    Remove old methods from AnalyticsClient

--- client/src/api/analytics.ts
+++ client/src/api/analytics.ts
@@ -1,14 +1,6 @@
 import { IHourlyVisitCount } from '../models/analytics.ts';
 
 export abstract class AnalyticsClient {
-    private static _padTimeValue(value: number): string {
-        return value.toString().padStart(2, '0');
-    }
-
-    public static getDateString(date: Date): string {
-        return `${date.getUTCFullYear()}-${AnalyticsClient._padTimeValue(date.getUTCMonth() + 1)}-${AnalyticsClient._padTimeValue(date.getUTCDate())}T${AnalyticsClient._padTimeValue(date.getUTCHours())}:${AnalyticsClient._padTimeValue(date.getUTCMinutes())}`;
-    }
-
     public static async retrieveHourlyVisitCountAsync(days: number): Promise<IHourlyVisitCount[]> {
         const response = await fetch(`/api/analytics/visits?days=${days}`);
         if (!response.ok) {
