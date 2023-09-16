8e1711d8fc7e925359c84525fcbdda40b817c93c
Spencer <spencer@frozor.io>
2023-09-16 12:40:06 -0700

    Fix empty home page in case of missing dining halls
    If you selected dining halls that are no longer available, the home may be blank. We now calculate available ids instead dynamically.

--- client/src/components/pages/home/home-page.tsx
+++ client/src/components/pages/home/home-page.tsx
@@ -1,14 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { SettingsContext } from '../../../context/settings.ts';
 import { Link } from 'react-router-dom';
 import { CombinedDiningHallMenuList } from '../../dining-halls/combined-dining-hall-menu-list.tsx';
 
 import './home.css';
+import { ApplicationContext } from '../../../context/app.ts';
 
 export const HomePage = () => {
+    const { diningHallsById } = useContext(ApplicationContext);
     const [{ homepageDiningHallIds }] = useContext(SettingsContext);
 
-    if (homepageDiningHallIds.size === 0) {
+    // Users may have added dining halls to their home set which are no temporarily unavailable
+    const [availableDiningHallIds, setAvailableDiningHallIds] = useState<Set<string>>(new Set());
+
+    useEffect(() => {
+        const newAvailableDiningHallIds = new Set<string>();
+        for (const diningHallId of homepageDiningHallIds) {
+            if (diningHallsById.has(diningHallId)) {
+                newAvailableDiningHallIds.add(diningHallId);
+            }
+        }
+        setAvailableDiningHallIds(newAvailableDiningHallIds);
+    }, [diningHallsById, homepageDiningHallIds]);
+
+    if (availableDiningHallIds.size === 0) {
         return (
             <div className="centered-content">
                 <div className="card centered">
