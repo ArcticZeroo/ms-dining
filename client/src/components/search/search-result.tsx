399e995c8674e7ebb818cc0056d6e2f38d58c126
Spencer <spencer@frozor.io>
2023-10-04 12:26:49 -0700

    Side indicator for entity type

--- client/src/components/search/search-result.tsx
+++ client/src/components/search/search-result.tsx
@@ -1,18 +1,35 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { getViewUrl } from '../../util/link.ts';
-import './search.css';
 import { ApplicationContext } from '../../context/app.ts';
 import { SettingsContext } from '../../context/settings.ts';
-import { getParentView } from '../../util/view';
-import { ISearchResult } from '../../models/search.ts';
+import { ISearchResult, SearchEntityType } from '../../models/search.ts';
+import { getViewUrl } from '../../util/link.ts';
+import { classNames } from '../../util/react';
 import { sortCafeIds } from '../../util/sorting.ts';
+import { getParentView } from '../../util/view';
+import './search.css';
+
+interface IEntityDisplayData {
+    className: string;
+    iconName: string;
+}
+
+const entityDisplayDataByType: Record<SearchEntityType, IEntityDisplayData> = {
+    [SearchEntityType.menuItem]: {
+        className: 'entity-menu-item',
+        iconName: 'lunch_dining'
+    },
+    [SearchEntityType.station]: {
+        className: 'entity-station',
+        iconName: 'restaurant'
+    }
+};
 
 interface ISearchResultProps {
     result: ISearchResult;
 }
 
-export const SearchResult: React.FC<ISearchResultProps> = ({ result: { name, cafeIds, imageUrl } }) => {
+export const SearchResult: React.FC<ISearchResultProps> = ({ result: { name, cafeIds, imageUrl, entityType } }) => {
     const { viewsById } = useContext(ApplicationContext);
     const [{ showImages, useGroups }] = useContext(SettingsContext);
     const [cafeIdsInOrder, setCafeIdsInOrder] = useState<Array<string>>([]);
@@ -21,35 +38,44 @@ export const SearchResult: React.FC<ISearchResultProps> = ({ result: { name, caf
         setCafeIdsInOrder(sortCafeIds(cafeIds));
     }, [cafeIds]);
 
+    const entityDisplayData = entityDisplayDataByType[entityType];
+
     return (
-        <div className="card horizontal search-result">
-            <div className="search-result-info">
-                <div className="title">{name}</div>
-                <div className="body search-result-hits">
-                    {
-                        cafeIdsInOrder.map(id => {
-                            const view = viewsById.get(id);
-
-                            if (!view) {
-                                return false;
-                            }
-
-                            const parentView = getParentView(viewsById, useGroups, view);
-
-                            return (
-                                <Link to={getViewUrl(parentView)} className="search-result-chip" key={view.value.id}>
-                                    {view.value.name}
-                                </Link>
-                            );
-                        })
-                    }
-                </div>
+        <div className="search-result">
+            <div className={classNames('search-result-type', entityDisplayData.className)}>
+                <span className="material-symbols-outlined">
+                    {entityDisplayData.iconName}
+                </span>
             </div>
-            {
-                (imageUrl && showImages) && (
-                    <img src={imageUrl} alt={name} className="search-result-image"/>
-                )
-            }
+            <div className="search-result-info">
+                <div className="search-result-info-header">
+                    <div className="search-result-name">{name}</div>
+                    <div className="search-result-hits">
+                        {
+                            cafeIdsInOrder.map(id => {
+                                const view = viewsById.get(id);
+
+                                if (!view) {
+                                    return false;
+                                }
+
+                                const parentView = getParentView(viewsById, useGroups, view);
+
+                                return (
+                                    <Link to={getViewUrl(parentView)} className="search-result-chip" key={view.value.id}>
+                                        {view.value.name}
+                                    </Link>
+                                );
+                            })
+                        }
+                    </div>
+                </div>
+                {
+                    (imageUrl && showImages) && (
+                                                 <img src={imageUrl} alt={name} className="search-result-image"/>
+                                             )
+                }
+            </div>
         </div>
     );
 };
