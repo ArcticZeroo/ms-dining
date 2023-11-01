d5f83a578e54f3fdbf2a100c2344d9f1164bd2a8
Spencer <spencer@frozor.io>
2023-11-01 08:39:35 -0700

    Begin moving search over to common

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,1 +1,3 @@
-export * as DateUtil from './date-util.js';
+export * as DateUtil from './util/date-util.js';
+export * as SearchUtil from './util/search-util.js';
+export * as SearchTypes from './models/search.js';
---
+++ common/src/models/search.ts
@@ -0,0 +1,29 @@
+export enum SearchEntityType {
+    menuItem = 'menuItem',
+    station = 'station',
+}
+
+export enum SearchMatchReason {
+    title = 'title',
+    description = 'description'
+}
+
+export const allSearchEntityTypes = [
+    SearchEntityType.menuItem,
+    SearchEntityType.station,
+];
+
+export enum SearchEntityFilterType {
+    all,
+    menuItem,
+    station
+}
+
+export interface ISearchResult {
+    entityType: SearchEntityType;
+    name: string;
+    description?: string;
+    imageUrl?: string;
+    locationDatesByCafeId: Map<string, Array<Date>>;
+    matchReasons: Set<SearchMatchReason>;
+}
