c1fe36375dc3bc2902566ac31086598e52918ab6
Spencer <spencer@frozor.io>
2023-10-31 18:49:40 -0700

    Don't replace today's menus unless there are none already

--- server/src/api/cafe/cache/boot.ts
+++ server/src/api/cafe/cache/boot.ts
@@ -7,66 +7,83 @@ import { scheduleWeeklyUpdateJob } from './weekly.js';
 import { cafeList } from '../../../constants/cafes.js';
 
 const repairMissingWeeklyMenusAsync = async () => {
-	logInfo('Repairing missing weekly menus...');
+    logInfo('Repairing missing weekly menus...');
 
-	let isRepairNeeded = false;
-	for (const i of yieldDaysInFutureForThisWeek()) {
-		const date = getNowWithDaysInFuture(i);
-		const isAnyMenuAvailableToday = await CafeStorageClient.isAnyMenuAvailableForDayAsync(toDateString(date));
-		if (!isAnyMenuAvailableToday) {
-			isRepairNeeded = true;
-			logInfo(`Repairing missing menu for ${toDateString(date)}`);
-			const updateSession = new DailyCafeUpdateSession(i);
-			await updateSession.populateAsync();
-		} else {
-			logInfo(`No repair needed for ${toDateString(date)}`);
-		}
-	}
+    let isRepairNeeded = false;
+    for (const i of yieldDaysInFutureForThisWeek()) {
+        const date = getNowWithDaysInFuture(i);
+        const isAnyMenuAvailableToday = await CafeStorageClient.isAnyMenuAvailableForDayAsync(toDateString(date));
+        if (!isAnyMenuAvailableToday) {
+            isRepairNeeded = true;
+            logInfo(`Repairing missing menu for ${toDateString(date)}`);
+            const updateSession = new DailyCafeUpdateSession(i);
+            await updateSession.populateAsync();
+        } else {
+            logInfo(`No repair needed for ${toDateString(date)}`);
+        }
+    }
 
-	if (!isRepairNeeded) {
-		logInfo('No missing weekly menus found');
-	}
+    if (!isRepairNeeded) {
+        logInfo('No missing weekly menus found');
+    }
 };
 
 const repairCafesWithoutMenusAsync = async () => {
-	logInfo('Checking for cafes without menus...');
+    logInfo('Checking for cafes without menus...');
 
-	let isRepairNeeded = false;
-	for (const cafe of cafeList) {
-		const isAnyAllowedMenuAvailable = CafeStorageClient.isAnyAllowedMenuAvailableForCafe(cafe.id);
+    let isRepairNeeded = false;
+    for (const cafe of cafeList) {
+        const isAnyAllowedMenuAvailable = CafeStorageClient.isAnyAllowedMenuAvailableForCafe(cafe.id);
 
-		if (isAnyAllowedMenuAvailable) {
-			continue;
-		}
+        if (isAnyAllowedMenuAvailable) {
+            continue;
+        }
 
-		isRepairNeeded = true;
+        isRepairNeeded = true;
 
-		// Clear the cafe from storage in case we have a bad config
-		await CafeStorageClient.deleteCafe(cafe.id);
+        // Clear the cafe from storage in case we have a bad config
+        await CafeStorageClient.deleteCafe(cafe.id);
 
-		logInfo(cafe.id, 'is missing allowed menus, attempting to repair...');
-		for (const i of yieldDaysInFutureForThisWeek()) {
-			try {
-				const updateSession = new DailyCafeUpdateSession(i);
-				await updateSession.populateAsync([cafe]);
-			} catch {
-				logError(`Failed to repair ${cafe.id}. Removing it from the list for now...`);
-				await CafeStorageClient.deleteCafe(cafe.id);
-				break;
-			}
-		}
-	}
+        logInfo(cafe.id, 'is missing allowed menus, attempting to repair...');
+        for (const i of yieldDaysInFutureForThisWeek()) {
+            try {
+                const updateSession = new DailyCafeUpdateSession(i);
+                await updateSession.populateAsync([cafe]);
+            } catch {
+                logError(`Failed to repair ${cafe.id}. Removing it from the list for now...`);
+                await CafeStorageClient.deleteCafe(cafe.id);
+                break;
+            }
+        }
+    }
 
-	if (!isRepairNeeded) {
-		logInfo('All cafes have at least one allowed menu!');
-	}
+    if (!isRepairNeeded) {
+        logInfo('All cafes have at least one allowed menu!');
+    }
 }
 
-export const performBootTasks = async () => {
-	await populateDailySessionsAsync();
-	await repairMissingWeeklyMenusAsync();
-	await repairCafesWithoutMenusAsync();
+const repairTodaySessionsAsync = async () => {
+    const now = new Date();
 
-	scheduleDailyUpdateJob();
-	scheduleWeeklyUpdateJob();
+    // Don't bother repairing today after 6pm if there is already a daily menu;
+    // In case I'm making server changes and need to restart the server, I don't
+    // want to clear history.
+    if (now.getHours() > 17) {
+        const isAnyMenuAvailableToday = await CafeStorageClient.isAnyMenuAvailableForDayAsync(toDateString(now));
+        if (isAnyMenuAvailableToday) {
+			logInfo('Skipping repair of today\'s sessions because it is after 5pm and there is already a menu for today');
+            return;
+        }
+    }
+
+	await populateDailySessionsAsync();
+};
+
+export const performBootTasks = async () => {
+    await repairTodaySessionsAsync();
+    await repairMissingWeeklyMenusAsync();
+    await repairCafesWithoutMenusAsync();
+
+    scheduleDailyUpdateJob();
+    scheduleWeeklyUpdateJob();
 };
