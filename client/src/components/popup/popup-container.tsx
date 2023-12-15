0fb768d42c78cc14cd1c3810383660bdb8561f15
Spencer <spencer@frozor.io>
2023-12-15 11:30:04 -0800

    Escape event

--- client/src/components/popup/popup-container.tsx
+++ client/src/components/popup/popup-container.tsx
@@ -1,12 +1,30 @@
 import { useValueNotifier } from '../../hooks/events.ts';
 import { PopupContext } from '../../context/modal.ts';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import './popup.css';
 
 export const PopupContainer = () => {
     const popupNotifier = useContext(PopupContext);
     const popup = useValueNotifier(popupNotifier);
+
+    useEffect(() => {
+        if (popup == null) {
+            return;
+        }
+
+        const onEscapePressed = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                popupNotifier.value = null;
+            }
+        };
+
+        document.addEventListener('keydown', onEscapePressed);
+
+        return () => {
+            document.removeEventListener('keydown', onEscapePressed);
+        };
+    }, [popupNotifier, popup]);
 
     if (popup == null) {
         return null;
