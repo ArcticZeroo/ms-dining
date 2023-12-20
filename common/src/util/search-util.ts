854a4d6dbd4c0d23e44bb603ccaa4f726f99eddc
Spencer <spnovick@microsoft.com>
2023-12-19 19:23:13 -0800

    Separate perfect matches (draft)

--- common/src/util/search-util.ts
+++ common/src/util/search-util.ts
@@ -101,8 +101,8 @@ const getSubstringScoreForMatchReason = (queryText: string, searchResult: ISearc
 
     const longestSequentialSubstringLength = getSubstringScore({
         isSequential:                     true,
-        perfectMatchWithoutBoundaryScore: 2,
-        perfectMatchScore:                5,
+        perfectMatchWithoutBoundaryScore: 3,
+        perfectMatchScore:                15,
         matchingText,
         queryText,
     });
@@ -114,8 +114,8 @@ const getSubstringScoreForMatchReason = (queryText: string, searchResult: ISearc
         queryText,
     });
 
-    const baseScore = (longestSequentialSubstringLength * 20)
-        + (longestNonSequentialSubstringLength * 2);
+    const baseScore = (longestSequentialSubstringLength * 50)
+        + longestNonSequentialSubstringLength;
 
     if (matchReason === SearchMatchReason.title) {
         return baseScore;
@@ -124,17 +124,27 @@ const getSubstringScoreForMatchReason = (queryText: string, searchResult: ISearc
     return baseScore * 0.75;
 };
 
-const computeScore = (cafePriorityOrder: string[] | undefined, searchResult: ISearchResult, queryText: string) => {
+const computeScore = (cafePriorityOrder: string[] | undefined, searchResult: ISearchResult, queryText: string, doSubstringScore: boolean) => {
     queryText = queryText.toLowerCase();
 
+    console.log('');
+    console.log('Computing score for', searchResult.name);
+
     let totalSubstringScore = 0;
-    for (const matchReason of searchResult.matchReasons) {
-        totalSubstringScore += getSubstringScoreForMatchReason(queryText, searchResult, matchReason);
+    if (doSubstringScore) {
+        for (const matchReason of searchResult.matchReasons) {
+            totalSubstringScore += getSubstringScoreForMatchReason(queryText, searchResult, matchReason);
+        }
+        totalSubstringScore /= searchResult.matchReasons.size;
     }
-    totalSubstringScore /= searchResult.matchReasons.size;
 
     const cafeRelevancyScore = getCafeRelevancyScore(searchResult, cafePriorityOrder);
     const dateRelevancyScore = getDateRelevancyScore(searchResult);
+
+    console.log('Substring score:', totalSubstringScore);
+    console.log('Cafe relevancy score:', cafeRelevancyScore);
+    console.log('Date relevancy score:', dateRelevancyScore);
+    console.log('');
 
     const baseScore = totalSubstringScore
         + cafeRelevancyScore
@@ -161,7 +171,7 @@ export const sortSearchResults = ({
                                   }: ISortSearchResultsParams): ISearchResult[] => {
     const searchResultScores = new Map<SearchEntityType, Map<string, number>>();
 
-    const getScore = (searchResult: ISearchResult) => {
+    const getScore = (searchResult: ISearchResult, doSubstringScore: boolean) => {
         if (!searchResultScores.has(searchResult.entityType)) {
             searchResultScores.set(searchResult.entityType, new Map());
         }
@@ -169,17 +179,52 @@ export const sortSearchResults = ({
         const searchResultScoresByItemName = searchResultScores.get(searchResult.entityType)!;
 
         if (!searchResultScoresByItemName.has(searchResult.name)) {
-            searchResultScoresByItemName.set(searchResult.name, computeScore(cafePriorityOrder, searchResult, queryText));
+            searchResultScoresByItemName.set(searchResult.name, computeScore(cafePriorityOrder, searchResult, queryText, doSubstringScore));
+
+            console.log('Computed score for', searchResult.name, 'as', searchResultScoresByItemName.get(searchResult.name));
         }
         return searchResultScoresByItemName.get(searchResult.name)!;
     };
 
-    searchResults.sort((resultA, resultB) => {
-        const scoreA = getScore(resultA);
-        const scoreB = getScore(resultB);
+    const sortGroup = (group: ISearchResult[], doSubstringScore: boolean) => {
+        group.sort((resultA, resultB) => {
+            const scoreA = getScore(resultA, doSubstringScore);
+            const scoreB = getScore(resultB, doSubstringScore);
 
-        return scoreB - scoreA;
-    });
+            return scoreB - scoreA;
+        });
+    }
 
-    return searchResults;
+    const perfectMatchRegex = new RegExp(`\\b${queryText.toLowerCase()}\\b`, 'i');
+
+    console.log(perfectMatchRegex);
+
+    const perfectTitleMatches: ISearchResult[] = [];
+    const perfectDescriptionMatches: ISearchResult[] = [];
+    const nonPerfectMatches: ISearchResult[] = [];
+
+    for (const searchResult of searchResults) {
+        if (searchResult.matchReasons.has(SearchMatchReason.title) && perfectMatchRegex.test(searchResult.name)) {
+            perfectTitleMatches.push(searchResult);
+        } else if (searchResult.matchReasons.has(SearchMatchReason.description) && perfectMatchRegex.test(searchResult.description)) {
+            perfectDescriptionMatches.push(searchResult);
+        } else {
+            nonPerfectMatches.push(searchResult);
+        }
+    }
+
+    // TODO: More metadata to do this all in one object
+    sortGroup(perfectTitleMatches, false);
+    sortGroup(perfectDescriptionMatches, false);
+    sortGroup(nonPerfectMatches, true);
+
+    console.log('Perfect title matches:', perfectTitleMatches.map((result) => result.name));
+    console.log('Perfect description matches:', perfectDescriptionMatches.map((result) => result.name));
+    console.log('Non-perfect matches:', nonPerfectMatches.map((result) => result.name));
+
+    return [
+        ...perfectTitleMatches,
+        ...perfectDescriptionMatches,
+        ...nonPerfectMatches
+    ];
 };
