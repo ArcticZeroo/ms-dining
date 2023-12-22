c7aeef8189b4991b9187565fc12a84a84dcac148
Spencer <spencer@frozor.io>
2023-12-22 11:00:46 -0800

    Fix problem with thumbnails not updating over time
    Ingredients in particular tends to update menu items when the menu changes rather than swapping for a new menu item. I also had this problem with some "coming soon" images for Just Poke.

--- server/src/api/cafe/image/thumbnail.ts
+++ server/src/api/cafe/image/thumbnail.ts
@@ -2,11 +2,13 @@ import Jimp from 'jimp';
 import { imageSize as getImageSizeSync } from 'image-size';
 import * as path from 'node:path';
 import * as fs from 'node:fs';
+import * as fsPromises from 'node:fs/promises';
 import { promisify } from 'node:util';
 import { serverMenuItemThumbnailPath } from '../../../constants/config.js';
 import { defaultUserAgent } from '../../../constants/http.js';
 import { IMenuItem } from '../../../models/cafe.js';
 import { runPromiseWithRetries } from '../../../util/async.js';
+import { logDebug } from '../../../util/log.js';
 
 const maxThumbnailHeightPx = 200;
 const maxThumbnailWidthPx = 400;
@@ -16,95 +18,129 @@ const loadImageRetryDelayMs = 1000;
 const getImageSizeAsync = promisify(getImageSizeSync);
 
 export const loadImageData = async (url: string): Promise<Buffer> => {
-	const response = await runPromiseWithRetries(
-		() => fetch(url, {
-			headers: {
-				'User-Agent': defaultUserAgent
-			}
-		}),
-		loadImageRetries,
-		loadImageRetryDelayMs
-	);
+    logDebug(`[Thumbnail] Loading image data from ${encodeURI(url)}`);
 
-	if (!response.ok) {
-		let text;
-		try {
-			text = await response.text();
-		} catch {
-			throw new Error(`Response failed with status: ${response.status}, could not deserialize text`);
-		}
-		throw new Error(`Response failed with status: ${response.status}, text: ${text}`);
-	}
+    const response = await runPromiseWithRetries(
+        () => fetch(url, {
+            headers: {
+                'User-Agent': defaultUserAgent
+            }
+        }),
+        loadImageRetries,
+        loadImageRetryDelayMs
+    );
 
-	const buffer = await response.arrayBuffer();
-	return Buffer.from(buffer);
+    if (!response.ok) {
+        let text;
+        try {
+            text = await response.text();
+        } catch {
+            throw new Error(`Response failed with status: ${response.status}, could not deserialize text`);
+        }
+        throw new Error(`Response failed with status: ${response.status}, text: ${text}`);
+    }
+
+    const buffer = await response.arrayBuffer();
+    return Buffer.from(buffer);
 };
 
 // static/menu-items/thumbnail/<id>
 export const getThumbnailFilepath = (id: string) => path.join(serverMenuItemThumbnailPath, `${id}.png`);
 
 interface IThumbnailData {
-	hasThumbnail: boolean;
-	thumbnailWidth?: number;
-	thumbnailHeight?: number;
+    hasThumbnail: boolean;
+    thumbnailWidth?: number;
+    thumbnailHeight?: number;
+    lastUpdateTime?: Date;
 }
 
 export const retrieveExistingThumbnailData = async (id: string, existingThumbnailData?: IThumbnailData): Promise<IThumbnailData> => {
-	const thumbnailPath = getThumbnailFilepath(id);
+    const thumbnailPath = getThumbnailFilepath(id);
 
-	if (!fs.existsSync(thumbnailPath)) {
-		return {
-			hasThumbnail: false
-		};
-	}
+    if (!fs.existsSync(thumbnailPath)) {
+        return {
+            hasThumbnail: false
+        };
+    }
 
-	if (existingThumbnailData?.thumbnailWidth && existingThumbnailData?.thumbnailHeight) {
-		return {
-			hasThumbnail:    true,
-			thumbnailWidth:  existingThumbnailData.thumbnailWidth,
-			thumbnailHeight: existingThumbnailData.thumbnailHeight
-		};
-	}
+    const { mtime: fileLastUpdateTime } = await fsPromises.stat(thumbnailPath);
 
-	try {
-		const { width, height } = await getImageSizeAsync(thumbnailPath);
-		return {
-			hasThumbnail:    true,
-			thumbnailWidth:  width,
-			thumbnailHeight: height
-		};
-	} catch (err) {
-		// Could not read file?
-		return {
-			hasThumbnail: false
-		};
-	}
+    if (existingThumbnailData?.hasThumbnail && existingThumbnailData?.thumbnailWidth && existingThumbnailData?.thumbnailHeight) {
+        return {
+            hasThumbnail:    true,
+            thumbnailWidth:  existingThumbnailData.thumbnailWidth,
+            thumbnailHeight: existingThumbnailData.thumbnailHeight,
+            lastUpdateTime:  fileLastUpdateTime
+        };
+    }
+
+    try {
+        const { width, height } = await getImageSizeAsync(thumbnailPath);
+        return {
+            hasThumbnail:    true,
+            thumbnailWidth:  width,
+            thumbnailHeight: height,
+            lastUpdateTime:  fileLastUpdateTime
+        };
+    } catch (err) {
+        // Could not read file?
+        return {
+            hasThumbnail: false
+        };
+    }
 };
 
+const thumbnailDataFromMenuItem = (menuItem: IMenuItem): IThumbnailData => {
+    if (menuItem.hasThumbnail) {
+        return {
+            hasThumbnail:    true,
+            thumbnailWidth:  menuItem.thumbnailWidth || 0,
+            thumbnailHeight: menuItem.thumbnailHeight || 0,
+            lastUpdateTime:  menuItem.lastUpdateTime || new Date(0)
+        };
+    } else {
+        return {
+            hasThumbnail: false
+        };
+    }
+};
+
+const isThumbnailUpToDate = (thumbnailData: IThumbnailData, menuItem: IMenuItem): boolean => {
+    if (!thumbnailData.hasThumbnail || !thumbnailData.lastUpdateTime) {
+        return false;
+    }
+
+    if (!menuItem.lastUpdateTime) {
+        return false;
+    }
+
+    return thumbnailData.lastUpdateTime.getTime() >= menuItem.lastUpdateTime.getTime();
+}
+
 export const createAndSaveThumbnailForMenuItem = async (menuItem: IMenuItem): Promise<void> => {
-	if (!menuItem.imageUrl || (menuItem.hasThumbnail && menuItem.thumbnailWidth && menuItem.thumbnailHeight)) {
-		return;
-	}
+    if (!menuItem.imageUrl || (menuItem.hasThumbnail && menuItem.thumbnailWidth && menuItem.thumbnailHeight)) {
+        return;
+    }
 
-	// May have been created on a previous day/run
-	const thumbnailData = await retrieveExistingThumbnailData(menuItem.id, menuItem /*existingThumbnailData*/);
-	if (thumbnailData.hasThumbnail) {
-		menuItem.hasThumbnail = true;
-		menuItem.thumbnailWidth = thumbnailData.thumbnailWidth;
-		menuItem.thumbnailHeight = thumbnailData.thumbnailHeight;
-		return;
-	}
+    // May have been created on a previous day/run
+    const thumbnailData = await retrieveExistingThumbnailData(menuItem.id, thumbnailDataFromMenuItem(menuItem));
+    if (thumbnailData.hasThumbnail && isThumbnailUpToDate(thumbnailData, menuItem)) {
+        menuItem.hasThumbnail = true;
+        menuItem.thumbnailWidth = thumbnailData.thumbnailWidth;
+        menuItem.thumbnailHeight = thumbnailData.thumbnailHeight;
+        return;
+    }
 
-	const imageData = await loadImageData(menuItem.imageUrl);
-	const image = await Jimp.read(imageData);
+    const imageData = await loadImageData(menuItem.imageUrl);
+    const image = await Jimp.read(imageData);
 
-	const { height, width } = image.bitmap;
-	const scale = Math.min(maxThumbnailHeightPx / height, maxThumbnailWidthPx / width);
+    const { height, width } = image.bitmap;
+    const scale = Math.min(maxThumbnailHeightPx / height, maxThumbnailWidthPx / width);
 
-	image.scale(scale);
+    image.scale(scale);
 
-	await image.writeAsync(getThumbnailFilepath(menuItem.id));
+    await image.writeAsync(getThumbnailFilepath(menuItem.id));
 
-	menuItem.thumbnailWidth = image.getWidth();
-	menuItem.thumbnailHeight = image.getHeight();
+    menuItem.thumbnailWidth = image.getWidth();
+    menuItem.thumbnailHeight = image.getHeight();
 };
