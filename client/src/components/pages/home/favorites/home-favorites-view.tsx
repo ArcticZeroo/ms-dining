d8f1ce1c3f85c5fde9554fb6b4f981e59915b728
Spencer <spencer@frozor.io>
2023-12-24 15:46:45 -0800

    Show favorites loading status in the collapse bit

---
+++ client/src/components/pages/home/favorites/home-favorites-view.tsx
@@ -0,0 +1,102 @@
+import { ExpandIcon } from '../../../icon/expand.tsx';
+import { HomeFavoriteResult } from './home-favorite-result.tsx';
+import React, { useCallback, useMemo, useState } from 'react';
+import { classNames } from '../../../../util/react.ts';
+import { useValueNotifierContext } from '../../../../hooks/events.ts';
+import { SelectedDateContext } from '../../../../context/time.ts';
+import { PromiseStage, useImmediatePromiseState } from '@arcticzeroo/react-promise-hook';
+import { ISearchQuery, SearchEntityType } from '@msdining/common/dist/models/search.js';
+import { DiningClient } from '../../../../api/dining.ts';
+import { isAnyDateToday } from '../../../../util/search.ts';
+
+const useFavoriteSearchResults = (favoriteItemNames: Set<string>) => {
+    const selectedDate = useValueNotifierContext(SelectedDateContext);
+
+    const retrieveFavoriteSearchResults = useCallback(async () => {
+        if (favoriteItemNames.size === 0) {
+            return [];
+        }
+
+        const queries: ISearchQuery[] = Array.from(favoriteItemNames).map(name => ({
+            text: name,
+            type: SearchEntityType.menuItem
+        }));
+
+        return DiningClient.retrieveFavoriteSearchResults(queries);
+    }, [favoriteItemNames]);
+
+    const { stage, value } = useImmediatePromiseState(retrieveFavoriteSearchResults);
+
+    const filteredResults = useMemo(
+        () => {
+            const results = value ?? [];
+            return results.filter(item => isAnyDateToday(item.locationDatesByCafeId, selectedDate));
+        },
+        [value, selectedDate]
+    );
+
+    return { stage, results: filteredResults } as const;
+}
+
+interface IHomeFavoritesViewProps {
+    names: Set<string>;
+}
+
+export const HomeFavoritesView: React.FC<IHomeFavoritesViewProps> = ({ names }) => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const selectedDate = useValueNotifierContext(SelectedDateContext);
+
+    const onToggleExpansion = () => {
+        setIsCollapsed(!isCollapsed);
+    }
+
+    const { stage, results } = useFavoriteSearchResults(names);
+
+    const bodyView = useMemo(() => {
+        if (stage === PromiseStage.running) {
+            return (
+                <div className="centered-content">
+                    <span className="loading-spinner"/>
+                    Loading favorites...
+                </div>
+            );
+        }
+
+        if (stage === PromiseStage.error) {
+            return (
+                <div className="error-card">
+                    Could not load favorites.
+                </div>
+            );
+        }
+
+        if (results.length === 0) {
+            return;
+        }
+
+        return results.map(result => (
+            <HomeFavoriteResult
+                key={result.name}
+                result={result}
+                date={selectedDate}
+            />
+        ));
+    }, [stage, results, selectedDate]);
+
+    return (
+        <div className="collapsible-content flex-col" id="home-favorites">
+            <div className="collapse-toggle" onClick={onToggleExpansion}>
+                <div className="flex-row">
+                    Favorites Across Campus on {selectedDate.toLocaleDateString()}
+                </div>
+                <ExpandIcon isExpanded={!isCollapsed}/>
+            </div>
+            <div
+                className={classNames('collapse-body', isCollapsed && 'collapsed')}
+                id="home-favorites-results"
+            >
+                {bodyView}
+            </div>
+        </div>
+    );
+};
--- client/src/components/pages/home/favorites/home-favorites.tsx
+++ client/src/components/pages/home/favorites/home-favorites.tsx
@@ -1,73 +1,15 @@
-import { useValueNotifier, useValueNotifierContext } from '../../../../hooks/events.ts';
+import { useValueNotifier } from '../../../../hooks/events.ts';
 import { ApplicationSettings } from '../../../../api/settings.ts';
-import { useCallback, useMemo } from 'react';
-import { DiningClient } from '../../../../api/dining.ts';
-import { ISearchQuery, SearchEntityType } from '@msdining/common/dist/models/search';
-import { PromiseStage, useImmediatePromiseState } from '@arcticzeroo/react-promise-hook';
-import { isAnyDateToday } from '../../../../util/search.ts';
-import { HomeFavoriteResultList } from './home-favorite-result-list.tsx';
-import { SelectedDateContext } from '../../../../context/time.ts';
-
-const useFavoriteSearchResults = (favoriteItemNames: Set<string>) => {
-    const selectedDate = useValueNotifierContext(SelectedDateContext);
-
-    const retrieveFavoriteSearchResults = useCallback(async () => {
-        if (favoriteItemNames.size === 0) {
-            return [];
-        }
-
-        const queries: ISearchQuery[] = Array.from(favoriteItemNames).map(name => ({
-            text: name,
-            type: SearchEntityType.menuItem
-        }));
-
-        return DiningClient.retrieveFavoriteSearchResults(queries);
-    }, [favoriteItemNames]);
-
-    const { stage, value } = useImmediatePromiseState(retrieveFavoriteSearchResults);
-
-    const filteredResults = useMemo(
-        () => {
-            const results = value ?? [];
-            return results.filter(item => isAnyDateToday(item.locationDatesByCafeId, selectedDate));
-        },
-        [value, selectedDate]
-    );
-
-    return { stage, results: filteredResults } as const;
-}
+import { HomeFavoritesView } from './home-favorites-view.tsx';
 
 export const HomeFavorites = () => {
     const favoriteItemNames = useValueNotifier(ApplicationSettings.favoriteItemNames);
-
-    const { stage, results } = useFavoriteSearchResults(favoriteItemNames);
 
     if (favoriteItemNames.size === 0) {
         return null;
     }
 
-    if (stage === PromiseStage.running) {
-        return (
-            <div className="card">
-                <span className="loading-spinner"/>
-                Loading favorites...
-            </div>
-        );
-    }
-
-    if (stage === PromiseStage.error) {
-        return (
-            <div className="error-card">
-                Could not load favorites.
-            </div>
-        );
-    }
-
-    if (results.length === 0) {
-        return;
-    }
-
     return (
-        <HomeFavoriteResultList results={results}/>
+        <HomeFavoritesView names={favoriteItemNames}/>
     );
 };
