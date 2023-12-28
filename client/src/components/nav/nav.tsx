28c63476c912e1fa63812e5d664f2b891de9863c
Spencer <spnovick@microsoft.com>
2023-12-28 15:46:05 -0800

    Increase size of nav + fix annoying gap
    It just annoyed me and no other reason

--- client/src/components/nav/nav.tsx
+++ client/src/components/nav/nav.tsx
@@ -1,31 +1,44 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useMemo } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { NavExpansionContext } from '../../context/nav.ts';
 
 import './nav.css';
-import { NavListHeaderItems } from './nav-header-buttons.tsx';
+import { DeviceType, useDeviceType } from '../../hooks/media-query.ts';
+import { classNames } from '../../util/react.ts';
 import { NavCafeList } from './nav-cafe-list.tsx';
+import { NavListHeaderItems } from './nav-header-buttons.tsx';
 
 export const Nav: React.FC = () => {
     const location = useLocation();
     const [isExpanded, setIsExpanded] = useContext(NavExpansionContext);
+    const deviceType = useDeviceType();
 
     useEffect(() => {
         setIsExpanded(false);
     }, [location.pathname, setIsExpanded]);
 
-    const visibilityToggleButton = (
-        <button onClick={() => setIsExpanded(!isExpanded)} className="visibility-toggle">
-            <span className="material-symbols-outlined">
-                menu
-            </span>
-        </button>
+    const visibilityToggleButton = useMemo(
+        () => {
+            if (deviceType === DeviceType.Desktop) {
+                return null;
+            }
+
+            return (
+                <a onClick={() => setIsExpanded(!isExpanded)} className="link-button visibility-toggle centered-content">
+                    <span className="material-symbols-outlined">
+                        menu
+                    </span>
+                </a>
+            );
+        },
+        [deviceType, isExpanded, setIsExpanded]
     );
 
     return (
-        <nav className={isExpanded ? 'expanded' : ''}>
+        <nav className={classNames(isExpanded && 'expanded', deviceType === DeviceType.Mobile && !isExpanded && 'collapsed')}>
             {!isExpanded && visibilityToggleButton}
             <ul id="sticky-header-list" className="expandable-nav-list">
+                {/* Move to here while expanded so that it is sticky with the rest of the nav */}
                 {isExpanded && visibilityToggleButton}
                 <NavListHeaderItems/>
             </ul>
