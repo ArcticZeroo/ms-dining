32a78e642fe7cb712320f1060960f50251b689e4
Spencer <spencer@frozor.io>
2024-01-05 22:16:40 -0800

    String values for number input

--- client/src/components/settings/number-setting-input.tsx
+++ client/src/components/settings/number-setting-input.tsx
@@ -1,6 +1,5 @@
 import { BooleanSetting, NumberSetting } from '../../api/settings.ts';
-import React, { useMemo } from 'react';
-import { useValueNotifierAsState } from '../../hooks/events.ts';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useAreRequiredSettingsEnabled } from '../../hooks/settings.ts';
 import { classNames } from '../../util/react.ts';
 
@@ -21,46 +20,78 @@ export const NumberSettingInput: React.FC<INumberSettingInputProps> = ({
     max,
     step
 }) => {
-    const [value, setValue] = useValueNotifierAsState(setting);
+    // We don't want to directly use the value notifier because it's really annoying when a website doesn't let a user
+    // directly edit a number input (e.g. you have 10 in the box, you want to write 11, but the min is 5 so when you 
+    // delete the zero it just returns). So, instead we will hold a string and only write to the value when it's valid.
+    const [valueRaw, setValueRaw] = useState<string>(() => setting.value.toString());
+
+    const isValid = useMemo(
+        () => {
+            const value = Number(valueRaw);
+
+            return !Number.isNaN(value)
+                && value >= min
+                && (max == null || value <= max);
+        },
+        [max, min, valueRaw]
+    );
 
     const areRequiredSettingsEnabled = useAreRequiredSettingsEnabled(requiredSettings);
-    const isAtLimit = value === min || (max != null && value === max);
+    const isAtLimit = useMemo(
+        () => {
+            if (!isValid) {
+                return false;
+            }
+            
+            const value = Number(valueRaw);
+
+            return value === min || (max != null && value === max);
+        },
+        [isValid, valueRaw, min, max]
+    );
+
+    useEffect(() => {
+        if (isValid) {
+            setting.value = Number(valueRaw);
+        }
+    }, [isValid, setting, valueRaw]);
 
     const onValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newValueRaw = event.target.value;
-        const newValue = Number(newValueRaw);
-
-        if (Number.isNaN(newValue)) {
-            return;
-        }
-
-        if (newValue < min) {
-            return;
-        }
-
-        if (max != null && newValue > max) {
-            return;
-        }
-
-        setValue(newValue);
+        setValueRaw(newValueRaw);
     };
 
     const title = useMemo(() => {
+        const value = Number(valueRaw);
+
+        if (Number.isNaN(value)) {
+            return 'Invalid number!';
+        }
+        
         if (value === min) {
             return 'Can\'t go any lower!';
+        }
+
+        if (value < min) {
+            return `Must be at least ${min}!`;
         }
 
         if (max != null && value === max) {
             return 'Can\'t go any higher!';
         }
-    }, [value, min, max]);
+
+        if (max != null && value > max) {
+            return `Must be at most ${max}!`;
+        }
+    }, [valueRaw, min, max]);
 
     return (
         <label
             className={classNames(
                 'setting-input setting-chip number-setting',
                 !areRequiredSettingsEnabled && 'disabled',
-                isAtLimit && 'at-limit'
+                isAtLimit && 'at-limit',
+                !isValid && 'invalid'
             )}
             title={title}
         >
@@ -72,7 +103,7 @@ export const NumberSettingInput: React.FC<INumberSettingInputProps> = ({
                 min={min}
                 max={max}
                 step={step}
-                value={value}
+                value={valueRaw}
                 onChange={onValueChange}
                 disabled={!areRequiredSettingsEnabled}
             />
