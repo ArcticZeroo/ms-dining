847519610a9abf6cbc211457aa5126d08049d524
Spencer <spencer@frozor.io>
2024-01-19 23:08:47 -0800

    More warning

--- client/src/components/notice/online-ordering-experimental.tsx
+++ client/src/components/notice/online-ordering-experimental.tsx
@@ -7,6 +7,13 @@ export const OnlineOrderingExperimental = () => {
                 <li>Your credit card might be charged but the order might not make it to the cafe</li>
                 <li>The cafe receives incorrect order information (or none at all)</li>
             </ul>
+            You probably shouldn't even use this feature while this warning exists!
+            <br/>
+            That said... please let me know whether your order went well (or not) so I can improve this feature.
+            <br/>
+            <a href="mailto:spnovick@microsoft.com" target="_blank">Email Me</a>
+            <a href="https://teams.microsoft.com/l/chat/0/0?users=spnovick@microsoft.com" target="_blank">Send me a Teams Message</a>
+
         </div>
     );
 }
