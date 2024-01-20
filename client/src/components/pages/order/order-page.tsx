d2e793fefc821aafc951beec2d6a7d53b28c6ae1
Spencer <spencer@frozor.io>
2024-01-19 21:20:44 -0800

    Don't auto navigate on empty cart
    If you remove your cart items, you shouldn't get auto redirected, that's weird.

--- client/src/components/pages/order/order-page.tsx
+++ client/src/components/pages/order/order-page.tsx
@@ -1,19 +1,26 @@
 import { useValueNotifier, useValueNotifierContext } from '../../../hooks/events.ts';
 import { CartContext } from '../../../context/cart.ts';
 import { ApplicationSettings } from '../../../api/settings.ts';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { CartContentsTable } from '../../order/cart/cart-contents-table.tsx';
 import { MultiCafeOrderWarning } from '../../order/multi-cafe-order-warning.tsx';
 import './order-page.css';
 
 export const OrderPage = () => {
-    const navigate = useNavigate();
     const allowOnlineOrdering = useValueNotifier(ApplicationSettings.allowOnlineOrdering);
     const cart = useValueNotifierContext(CartContext);
 
-    if (!allowOnlineOrdering || cart.size === 0) {
-        navigate('/');
-        return;
+    const isCheckoutAllowed = allowOnlineOrdering && cart.size > 0;
+
+    if (!isCheckoutAllowed) {
+        return (
+            <div className="error-card">
+                You must add items to your cart before checking out.
+                <Link to="/" className="button">
+                    Go Home
+                </Link>
+            </div>
+        );
     }
 
     return (
