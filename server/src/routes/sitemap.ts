56d8ed6fa9d22f862a548d93609b2a4b697cb881
Spencer <spencer@frozor.io>
2024-02-02 16:04:54 -0800

    fix sitemap 404

--- server/src/routes/sitemap.ts
+++ server/src/routes/sitemap.ts
@@ -1,10 +1,11 @@
 import Router from '@koa/router';
 import { generateSitemap } from '../api/sitemap.js';
+import { streamToPromise } from 'sitemap';
 
 export const registerSitemapRoutes = (router: Router) => {
     router.get('/sitemap.xml', async (ctx) => {
         const sitemap = generateSitemap();
-        ctx.respond = false;
-        sitemap.pipe(ctx.res);
+        ctx.type = 'xml';
+        ctx.body = (await streamToPromise(sitemap)).toString('utf-8');
     });
 }
