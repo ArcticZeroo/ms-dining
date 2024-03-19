9630aef81dc6cddf6d6345ab82d65cff0a1f315d
Spencer <spencer@frozor.io>
2024-03-18 20:25:39 -0700

    Put homepage views on top when none are selected
    Favorites are less important on initial load.

--- client/src/components/settings/homepage-settings.tsx
+++ client/src/components/settings/homepage-settings.tsx
@@ -2,21 +2,36 @@ import React from 'react';
 import { BooleanSettingInput } from './boolean-setting-input.tsx';
 import { HomepageViewsSetting } from './homepage-views-setting.tsx';
 import { ApplicationSettings } from '../../constants/settings.ts';
+import { useValueNotifier } from '../../hooks/events.ts';
 
 interface IHomepageSettingsProps {
 	requireButtonToCommitHomepageViews: boolean;
 }
 
-export const HomepageSettings: React.FC<IHomepageSettingsProps> = ({ requireButtonToCommitHomepageViews }) => (
-    <div className="card settings-group">
-        <div className="title">
-			Homepage Setup
-        </div>
-        <BooleanSettingInput
-            icon="star"
-            name="Show Favorites on Homepage"
-            description="When enabled, the home page will search for items that are in your favorites list."
-            setting={ApplicationSettings.showFavoritesOnHome}/>
+export const HomepageSettings: React.FC<IHomepageSettingsProps> = ({ requireButtonToCommitHomepageViews }) => {
+    const homepageViewIds = useValueNotifier(ApplicationSettings.homepageViews);
+    const hasAnyHomepageViews = homepageViewIds.size > 0;
+
+    const homepageViewsComponent = (
         <HomepageViewsSetting requireButtonToCommit={requireButtonToCommitHomepageViews}/>
-    </div>
-);
+    );
+
+    return (
+        <div className="card settings-group">
+            <div className="title">
+                Homepage Setup
+            </div>
+            {
+                !hasAnyHomepageViews && homepageViewsComponent
+            }
+            <BooleanSettingInput
+                icon="star"
+                name="Show Favorites on Homepage"
+                description="When enabled, the home page will search for items that are in your favorites list."
+                setting={ApplicationSettings.showFavoritesOnHome}/>
+            {
+                hasAnyHomepageViews && homepageViewsComponent
+            }
+        </div>
+    );
+};
