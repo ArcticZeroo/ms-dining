a05752a8d1dfee31ef3d499780a5ba3027572b62
Spencer <spencer@frozor.io>
2024-03-20 12:34:09 -0700

    automatically redirect users to menus from 404 page
    /foodhall4 should redirect to /menu/foodhall4

--- client/src/components/pages/not-found/not-found-page.tsx
+++ client/src/components/pages/not-found/not-found-page.tsx
@@ -1,11 +1,22 @@
-import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
 import { setPageData } from '../../../util/title.ts';
+import { ApplicationContext } from '../../../context/app.ts';
 
 export const NotFoundPage = () => {
+    const { viewsById } = useContext(ApplicationContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const view = viewsById.get(location.pathname.slice(1));
+
     useEffect(() => {
+        if (view != null) {
+            return navigate(`/menu/${view.value.id}`);
+        }
+
         setPageData('Page Not Found', 'The page you are looking for does not exist');
-    }, []);
+    }, [navigate, view]);
 
     return (
         <div className="error-card">
