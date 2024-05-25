e42590da1434965cc772cf28895de2b96bb7fae5
Spencer <spencer@frozor.io>
2024-05-24 22:10:31 -0700

    hide cart if OO is disabled

--- client/src/components/order/cart/cart-popup.tsx
+++ client/src/components/order/cart/cart-popup.tsx
@@ -3,16 +3,18 @@ import { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext, CartHydrationContext } from '../../../context/cart.ts';
 import { useIsTodaySelected } from '../../../hooks/date-picker.tsx';
-import { useValueNotifierContext } from '../../../hooks/events.ts';
+import { useValueNotifier, useValueNotifierContext } from '../../../hooks/events.ts';
 import { useScrollbarWidth } from '../../../hooks/scrollbar-size.ts';
 import { classNames } from '../../../util/react.ts';
 import { WaitTime } from '../wait-time.tsx';
 import { CartContentsTable } from './cart-contents-table.tsx';
 import { CartHydrationView } from './cart-hydration-view.tsx';
+import { DebugSettings } from '../../../constants/settings.ts';
 
 import './cart-popup.css';
 
 export const CartPopup = () => {
+    const allowOnlineOrdering = useValueNotifier(DebugSettings.allowOnlineOrdering);
     const cart = useValueNotifierContext(CartContext);
     const cartHydration = useValueNotifierContext(CartHydrationContext);
     const isTodaySelected = useIsTodaySelected();
@@ -25,6 +27,10 @@ export const CartPopup = () => {
 
     const hasMissingItems = cartHydration.missingItemsByCafeId != null && cartHydration.missingItemsByCafeId.size > 0;
     const shouldShow = isTodaySelected && (totalItemCount > 0 || hasMissingItems || cartHydration.stage === PromiseStage.running);
+
+    if (!allowOnlineOrdering) {
+        return;
+    }
 
     return (
         <div
