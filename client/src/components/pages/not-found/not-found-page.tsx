fb7be16d7044ecb0ce7c417042834fd64b0d8ebb
Spencer <spencer@frozor.io>
2024-05-24 22:05:32 -0700

    just redirect to home on not foundc

--- client/src/components/pages/not-found/not-found-page.tsx
+++ client/src/components/pages/not-found/not-found-page.tsx
@@ -1,6 +1,5 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useContext, useEffect } from 'react';
-import { setPageData } from '../../../util/title.ts';
 import { ApplicationContext } from '../../../context/app.ts';
 
 export const NotFoundPage = () => {
@@ -15,17 +14,6 @@ export const NotFoundPage = () => {
             return navigate(`/menu/${view.value.id}`);
         }
 
-        setPageData('Page Not Found', 'The page you are looking for does not exist');
+        return navigate('/');
     }, [navigate, view]);
-
-    return (
-        <div className="error-card">
-            <div>
-                Page not found!
-            </div>
-            <Link to="/">
-                Navigate Home
-            </Link>
-        </div>
-    );
 };
