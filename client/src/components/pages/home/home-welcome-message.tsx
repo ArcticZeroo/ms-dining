93fa0080f69dddb60a605e04129ea31d87843ccf
Spencer <spencer@frozor.io>
2024-06-15 14:10:06 -0700

    old home welcome message

--- client/src/components/pages/home/home-welcome-message.tsx
+++ client/src/components/pages/home/home-welcome-message.tsx
@@ -1,11 +1,26 @@
+import { DeviceType, useDeviceType } from '../../../hooks/media-query.ts';
+
 export const HomeWelcomeMessage = () => {
+    const deviceType = useDeviceType();
+
     return (
-        <div className="card centered-content">
+        <div className="card theme centered-content" id="home-welcome-message">
             <div className="title">
                 Welcome to MSDining! 🎉
             </div>
             <div className="subtitle">
                 View cafe menus, search for items across campus, easily find favorites, and more!
+            </div>
+            <div className="flex">
+                <span className="material-symbols-outlined">
+                    restaurant
+                </span>
+                <div>
+                    <span>
+                        View today's lunch menu by selecting a cafe from
+                        {deviceType === DeviceType.Mobile ? ' the ☰ icon above' : ' the sidebar on the left'}.
+                    </span>
+                </div>
             </div>
         </div>
     );
