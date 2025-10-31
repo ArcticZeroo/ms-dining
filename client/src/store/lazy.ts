c66cffacc6c155be3ba15e38f6f7c82e55fcb21b
Spencer <spencer@frozor.io>
2025-10-30 20:13:00 -0700

    never allow empty promise in lazy resource
    seems to break something?

--- client/src/store/lazy.ts
+++ client/src/store/lazy.ts
@@ -10,24 +10,26 @@ export class LazyResource<T> {
     private _idSymbol = Symbol();
     private readonly _factory: () => MaybePromise<T>;
     private readonly _runForPromiseState = () => this.get(true /*forceRefresh*/);
-    private readonly _state: ValueNotifier<ILazyPromiseState<T>> = new ValueNotifier<ILazyPromiseState<T>>({
-        run: this._runForPromiseState,
-        stage: PromiseStage.notRun,
-        value: undefined,
-        error: undefined,
-        promise: Promise.reject(new Error('Promise not yet started')).catch()
-    });
+    private _state: ValueNotifier<ILazyPromiseState<T>> | undefined = undefined;
 
     constructor(factory: () => MaybePromise<T>) {
         this._factory = factory;
     }
 
     get hasBeenRun(): boolean {
-        return this._state.value.stage !== PromiseStage.notRun;
+        return this._state != null && this._state.value.stage !== PromiseStage.notRun;
     }
 
     async _factoryAsPromise() {
         return this._factory();
+    }
+
+    #assignState(state: ILazyPromiseState<T>): void {
+        if (!this._state) {
+            this._state = new ValueNotifier<ILazyPromiseState<T>>(state);
+        } else {
+            this._state.value = state;
+        }
     }
 
     async _runFactory(promise: Promise<T>) {
@@ -42,64 +44,58 @@ export class LazyResource<T> {
                 return;
             }
 
-            this._state.value = {
+            this.#assignState({
                 run: this._runForPromiseState,
                 stage: PromiseStage.success,
                 value: result,
                 error: undefined,
                 promise
-            };
+            });
         } catch (error) {
             if (this._idSymbol !== mySymbol) {
                 return;
             }
 
-            this._state.value = {
+            this.#assignState({
                 run: this._runForPromiseState,
                 stage: PromiseStage.error,
                 value: undefined,
                 error,
                 promise,
-            };
+            });
         }
     }
 
     get(forceRefresh: boolean = false): ValueNotifier<ILazyPromiseState<T>> {
-        if (forceRefresh || this._state.value.stage === PromiseStage.notRun) {
+        if (forceRefresh || !this.hasBeenRun) {
             const promise = this._factoryAsPromise();
-            this._state.value = {
+            this.#assignState({
                 run: this._runForPromiseState,
                 stage: PromiseStage.running,
                 value: undefined,
                 error: undefined,
                 promise
-            };
+            });
 
             this._runFactory(promise)
                 .catch(err => console.error('Could not run factory in lazy resource:', err));
         }
 
+        if (!this._state) {
+            throw new Error('Lazy resource should have state after running factory');
+        }
+
         return this._state;
     }
 
-    clear() {
-        this._state.value = {
-            run: this._runForPromiseState,
-            stage: PromiseStage.notRun,
-            value: undefined,
-            error: undefined,
-            promise: Promise.reject(new Error('Promise not yet started'))
-        };
-    }
-
     set(value: T) {
-        this._state.value = {
+        this.#assignState({
             run:     this._runForPromiseState,
             stage:   PromiseStage.success,
             value,
             error:   undefined,
             promise: Promise.resolve(value)
-        };
+        });
     }
 
     async update(work: (value: T) => MaybePromise<T>, skipUpdateIfNotRun: boolean = false) {
