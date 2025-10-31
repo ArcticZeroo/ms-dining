287f1bba211021d692bee173140537c9fb8c1ae8
Spencer <spencer@frozor.io>
2025-10-30 18:53:06 -0700

    add station to menu items migration

---
+++ server/prisma/migrations/20251029033938_menu_item_station/migration.sql
@@ -0,0 +1,40 @@
+/*
+  Warnings:
+
+  - Added the required column `stationId` to the `MenuItem` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_MenuItem" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "normalizedName" TEXT NOT NULL DEFAULT '',
+    "description" TEXT,
+    "imageUrl" TEXT,
+    "tags" TEXT,
+    "calories" INTEGER NOT NULL,
+    "maxCalories" INTEGER NOT NULL,
+    "price" REAL NOT NULL,
+    "groupId" TEXT,
+    "externalLastUpdateTime" DATETIME,
+    "externalReceiptText" TEXT,
+    "cafeId" TEXT NOT NULL,
+    "stationId" TEXT NOT NULL,
+    CONSTRAINT "MenuItem_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "CrossCafeGroup" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "MenuItem_cafeId_fkey" FOREIGN KEY ("cafeId") REFERENCES "Cafe" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "MenuItem_stationId_fkey" FOREIGN KEY ("stationId") REFERENCES "Station" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_MenuItem" ("cafeId", "calories", "description", "externalLastUpdateTime", "externalReceiptText", "groupId", "id", "imageUrl", "maxCalories", "name", "normalizedName", "price", "tags", "stationId")
+SELECT MenuItem.cafeId, MenuItem.calories, MenuItem.description, MenuItem.externalLastUpdateTime, MenuItem.externalReceiptText, MenuItem.groupId, MenuItem.id, MenuItem.imageUrl, MenuItem.maxCalories, MenuItem.name, MenuItem.normalizedName, MenuItem.price, MenuItem.tags, Station.id
+FROM "MenuItem"
+    INNER JOIN DailyMenuItem ON DailyMenuItem.menuItemId = MenuItem.id
+    INNER JOIN DailyCategory ON DailyCategory.id = DailyMenuItem.categoryId
+    INNER JOIN DailyStation ON DailyStation.id = DailyCategory.stationId
+    INNER JOIN Station on Station.id = DailyStation.stationId
+GROUP BY MenuItem.id;
+DROP TABLE "MenuItem";
+ALTER TABLE "new_MenuItem" RENAME TO "MenuItem";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
