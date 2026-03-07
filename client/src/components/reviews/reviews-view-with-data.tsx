59a69551be8103995b6e247810306a969afa25c4
Spencer <spencer@frozor.io>
2026-03-07 15:17:40 -0800

    remove local data stuff since we have a store now

--- client/src/components/reviews/reviews-view-with-data.tsx
+++ client/src/components/reviews/reviews-view-with-data.tsx
@@ -2,58 +2,10 @@ import { PostReviewInput } from './post-review-input.tsx';
 import { ReviewStats } from './review-stats.tsx';
 import { ReviewsList } from './reviews-list.tsx';
 import { IReviewSummary } from '@msdining/common/models/review';
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { useIsLoggedIn } from '../../hooks/auth.ts';
 import { LogInForReviewButton } from './log-in-for-review-button.tsx';
 import { IReviewLookup } from '../../models/reviews.js';
-
-interface IReviewStats {
-    counts: Record<number, number>;
-    totalCount: number;
-    overallRating: number;
-}
-
-const useLocalStats = (reviewData: IReviewSummary, localRating: number | undefined): IReviewStats => {
-    return useMemo(
-        () => {
-            if (localRating == null || reviewData.myReview?.rating === localRating) {
-                return reviewData;
-            }
-
-            const counts = { ...reviewData.counts };
-            let totalCount = 0;
-            let overallRating = 0;
-
-            if (reviewData.myReview != null) {
-                if (reviewData.myReview.rating in counts) {
-                    counts[reviewData.myReview.rating] -= 1;
-                } else {
-                    console.error('missing count data for my review\'s count');
-                }
-            }
-
-            counts[localRating] = (counts[localRating] ?? 0) + 1;
-
-            for (let i = 1; i <= 10; i++) {
-                if (i in counts) {
-                    totalCount += counts[i]!;
-                    overallRating += i * counts[i]!;
-                }
-            }
-
-            if (totalCount > 0) {
-                overallRating /= totalCount;
-            }
-
-            return {
-                counts,
-                overallRating,
-                totalCount
-            };
-        },
-        [reviewData, localRating]
-    );
-};
 
 interface IReviewsViewWithDataProps {
     response: IReviewSummary;
@@ -66,8 +18,6 @@ export const ReviewsViewWithData: React.FC<IReviewsViewWithDataProps> = ({ respo
     const [localRating, setLocalRating] = useState<number>(response.myReview?.rating ?? 0);
     const [reviewId, setReviewId] = useState<string | undefined>(response.myReview?.id);
     const isLoggedIn = useIsLoggedIn();
-
-    const stats = useLocalStats(response, localRating);
 
     const entityId = lookup.menuItemId ?? lookup.stationId;
 
@@ -95,14 +45,14 @@ export const ReviewsViewWithData: React.FC<IReviewsViewWithDataProps> = ({ respo
                 }
                 <LogInForReviewButton/>
                 <ReviewStats
-                    counts={stats.counts}
-                    totalCount={stats.totalCount}
-                    overallRating={stats.overallRating}
+                    counts={response.counts}
+                    totalCount={response.totalCount}
+                    overallRating={response.overallRating}
                 />
             </div>
             <div className="flex flex-wrap">
                 <ReviewsList
-                    totalCount={stats.totalCount}
+                    totalCount={response.totalCount}
                     reviewsWithComments={response.reviewsWithComments}
                     entityId={entityId}
                     isStation={lookup.stationId != null}
