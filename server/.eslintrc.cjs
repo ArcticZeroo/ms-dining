93093efc8558c6cc556bc37da486057532baf5ba
Spencer <spencer@frozor.io>
2026-03-12 22:47:34 -0700

    add eslint

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    env: { node: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    rules: {
+        'indent': ['error', 4],
+        'curly': ['error', 'all'],
+        'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+        'prefer-const': 'error',
+    },
+};
