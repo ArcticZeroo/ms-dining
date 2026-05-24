bdf043cace928401c0e35d225aeeb718256ee58c
Spencer <spencer@frozor.io>
2026-05-23 22:23:38 -0700

    separate out completed order item component

--- client/src/components/pages/order/status/completed-orders-list.tsx
+++ client/src/components/pages/order/status/completed-orders-list.tsx
@@ -1,22 +1,15 @@
 import type { ICafeOrder } from '@msdining/common/models/order';
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { ApplicationContext } from '../../../../context/app.ts';
-import { getViewName } from '../../../../util/cafe.ts';
-import { getViewMenuUrlDirect } from '../../../../util/link.ts';
-import { formatEstimatedReadyTime } from '../../../../util/order.ts';
-import { formatTimeToHoursMinutes } from '../../../../util/date.js';
+import React from 'react';
 import { useReorder } from '../../../../hooks/reorder.ts';
-import { CompletedOrderItemsTable } from './completed-order-items-table.tsx';
 import { useServerCartHasAvailableItems } from '../../../../store/zustand/server-cart.js';
 import { pluralize } from '../../../../util/string.js';
+import { CompletedOrderItem } from '../completed-order-item.js';
 
 interface ICompletedOrdersListProps {
     orders: ICafeOrder[];
 }
 
 export const CompletedOrdersList: React.FC<ICompletedOrdersListProps> = ({ orders }) => {
-    const { viewsById } = useContext(ApplicationContext);
     const { reorder, isPending } = useReorder();
     const cartAlreadyHasAvailableItems = useServerCartHasAvailableItems();
 
@@ -69,45 +62,14 @@ export const CompletedOrdersList: React.FC<ICompletedOrdersListProps> = ({ order
                 </span>
             </div>
             <div className="flex flex-center flex-wrap">
-                {orders.map((order) => {
-                    const view = viewsById.get(order.cafeId);
-                    const cafeName = view == null ? order.cafeId : getViewName({ view, showGroupName: true });
-
-                    return (
-                        <div key={order.id} className="card bg-raised-2">
-                            <div className="title">
-                                {
-                                    view != null
-                                        ? <Link to={getViewMenuUrlDirect(view)}>{cafeName}</Link>
-                                        : cafeName
-                                }
-                            </div>
-                            <div>Order #{order.buyOnDemandOrderNumber}</div>
-                            <div>Sent to kitchen at {formatTimeToHoursMinutes(order.completedAt)}</div>
-                            <div>Estimated ready: {formatEstimatedReadyTime(order.completedAt, order.waitTimeMin, order.waitTimeMax)}</div>
-                            <CompletedOrderItemsTable
-                                items={order.items}
-                                subtotal={order.subtotal}
-                                tax={order.tax}
-                                total={order.total}
-                            />
-                            <button
-                                className="default-container default-button"
-                                disabled={isPending}
-                                onClick={() => reorder(order.items, false /*navigateAfterAdd*/)}
-                            >
-                                Add Items To Cart
-                            </button>
-                            <button
-                                className="default-container default-button"
-                                disabled={isPending}
-                                onClick={() => reorder(order.items)}
-                            >
-                                Reorder
-                            </button>
-                        </div>
-                    );
-                })}
+                {orders.map((order) => (
+                    <CompletedOrderItem
+                        key={order.id}
+                        order={order}
+                        reorder={reorder}
+                        isPending={isPending}
+                    />
+                ))}
             </div>
         </>
     );
