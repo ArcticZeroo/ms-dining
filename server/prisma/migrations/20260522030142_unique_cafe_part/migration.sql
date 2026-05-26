2ea20fd36e1bef614c59e1f15a784d4a106f37f8
Spencer <spencer@frozor.io>
2026-05-25 22:32:08 -0700

    remove intermediate migrations to push just one

--- server/prisma/migrations/20260522030142_unique_cafe_part/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[orderSessionId,cafeId]` on the table `OrderCafePart` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "OrderCafePart_orderSessionId_cafeId_key" ON "OrderCafePart"("orderSessionId", "cafeId");
