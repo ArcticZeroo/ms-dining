cc36aafddef289d275ca67b54844af48326e6b70
Spencer <spencer@frozor.io>
2026-08-30 22:14:36 -0700

    fix home reviews component

--- client/src/components/pages/home/reviews/home-recent-reviews.tsx
+++ client/src/components/pages/home/reviews/home-recent-reviews.tsx
@@ -1,9 +1,16 @@
 import { HomeCollapse } from '../home-collapse.tsx';
 import { HomeSettings } from '../../../../constants/settings.ts';
+import { useValueNotifier } from '../../../../hooks/events.ts';
 import { LogInForReviewButton } from '../../../reviews/log-in-for-review-button.tsx';
 import { HomeRecentReviewsView } from './home-recent-reviews-view.tsx';
 
 export const HomeRecentReviews = () => {
+    const showRecentReviews = useValueNotifier(HomeSettings.showRecentReviewsOnHome);
+
+    if (!showRecentReviews) {
+        return null;
+    }
+
     return (
         <HomeCollapse title="Recent Reviews" featureToggle={HomeSettings.showRecentReviewsOnHome}>
             <div className="flex-col">
